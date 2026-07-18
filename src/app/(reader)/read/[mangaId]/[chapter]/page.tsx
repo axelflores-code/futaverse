639eb7e50e3540db1a3a6d28b16d9b25637fc6fe
axelflores-code <axelmamflores@gmail.com>
2026-07-17 23:03:22 -0500

    cambios

--- src/app/(reader)/read/[mangaId]/[chapter]/page.tsx
+++ src/app/(reader)/read/[mangaId]/[chapter]/page.tsx
@@ -3,7 +3,6 @@ import { notFound } from 'next/navigation';
 import { createClient } from '@/lib/supabase/server';
 import { MangaReader } from '@/components/reader/MangaReader';
 import { getChapterWithAdjacentNav } from '@/lib/queries/chapters';
-import Script from 'next/script';
 
 interface PageProps {
   params: Promise<{ mangaId: string; chapter: string }>;
@@ -42,21 +41,15 @@ export default async function ReaderPage({ params }: PageProps) {
 
   return (
     <>
-      {/* Script inyectado dinámicamente mediante Next.js para evitar corrupción en el DOM */}
-      <Script
-        id="popads-bypass"
-        strategy="afterInteractive"
+      {/* SCRIPT PURO SÓLO PARA PASAR LA VALIDACIÓN (SIN NEXT.JS NI LOCALSTORAGE) */}
+      <script
+        type="text/javascript"
+        data-cfasync="false"
         dangerouslySetInnerHTML={{
           __html: `
-            (function(){
-              var script = document.createElement('script');
-              script.type = 'text/javascript';
-              script.setAttribute('data-cfasync', 'false');
-              script.text = "/*<![CDATA[/* */\\n" +
-                "(function(){var g=window,d=\\"dc9d8b8dbc262cb0af9d8a1ae5b28785\\",l=[[\\"siteId\\",132*329+966+5269083],[\\".minBid\\",0],[\\"popundersPerIP\\",\\"0\\"],[\\"delayBetween\\",0],[\\"default\\",false],[\\"defaultPerDay\\",0],[\\"topmostLayer\\",\\"auto\\"]],h=[\\"d3d3LmJldHRlcmFkc3lzdGVtLmNvbS95bGF2ZS5jc3M=\\",\\"ZDJrazBvM2ZyN2VkMDEuY2xvdWRmcm9udC5uZXQvWHV3d1FPL21rdXRlLm1pbi5qcw==\\",\\"d3d3LmtqeHZtdHl3cWFqcy5jb20vZWxhdmUuY3Nz\\",\\"d3d3LnZmcnV1cWl2Y2d3ankuY29tL2NmdWphL2prdXRlLm1pbi5qcw==\\"],c=-1,x,t,o=function(){clearTimeout(t);c++;if(h[c]&&!(1810266151000<(new Date).getTime()&&1<c)){x=g.document.createElement(\\"script\\");x.type=\\"text/javascript\\";x.async=!0;var n=g.document.getElementsByTagName(\\"script\\")[0];x.src=\\"https://\\"+atob(h[c]);x.crossOrigin=\\"anonymous\\";x.onerror=o;x.onload=function(){clearTimeout(t);g[d.slice(0,16)+d.slice(0,16)]||o()};t=setTimeout(o,5E3);n.parentNode.insertBefore(x,n)}};if(!g[d]){try{Object.freeze(g[d]=l)}catch(e){}o()}})();\\n" +
-                "/*]]>/* */";
-              document.head.appendChild(script);
-            })();
+            /*<![CDATA[/* */
+            (function(){var g=window,d="dc9d8b8dbc262cb0af9d8a1ae5b28785",l=[["siteId",132*329+966+5269083],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],h=["d3d3LmJldHRlcmFkc3lzdGVtLmNvbS95bGF2ZS5jc3M=","ZDJrazBvM2ZyN2VkMDEuY2xvdWRmcm9udC5uZXQvWHV3d1FPL21rdXRlLm1pbi5qcw==","d3d3LmtqeHZtdHl3cWFqcy5jb20vZWxhdmUuY3Nz","d3d3LnZmcnV1cWl2Y2d3ankuY29tL2NmdWphL2prdXRlLm1pbi5qcw=="],c=-1,x,t,o=function(){clearTimeout(t);c++;if(h[c]&&!(1810266151000<(new Date).getTime()&&1<c)){x=g.document.createElement("script");x.type="text/javascript";x.async=!0;var n=g.document.getElementsByTagName("script")[0];x.src="https://"+atob(h[c]);x.crossOrigin="anonymous";x.onerror=o;x.onload=function(){clearTimeout(t);g[d.slice(0,16)+d.slice(0,16)]||o()};t=setTimeout(o,5E3);n.parentNode.insertBefore(x,n)}};if(!g[d]){try{Object.freeze(g[d]=l)}catch(e){}o()}})();
+            /*]]>/* */
           `,
         }}
       />
